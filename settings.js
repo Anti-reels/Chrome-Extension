0640e1e0fa346d64039ed5ff0850157284bfe09f
iInvisibilities <abuserod@gmail.com>
2025-04-20 17:20:23 +0100

    Add css

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const checkBoxEl = document.getElementById("checkbox1");
 chrome.storage.local.get(["is_blocking"]).then((result) => {
-    if (result) checkBoxEl.checked = result.is_blocking;
+    if (result) checkBoxEl.checked = !result.is_blocking;
 });
-checkBoxEl.addEventListener("input", async (e) => await chrome.storage.local.set({ is_blocking: checkBoxEl.checked }));
+checkBoxEl.addEventListener("input", async (e) => await chrome.storage.local.set({ is_blocking: !checkBoxEl.checked }));
