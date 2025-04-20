3fb19d25a8a55197515724184080069e267abd4d
iInvisibilities <abuserod@gmail.com>
2025-04-20 14:54:33 +0100

    Add toggle between allow/disallow reels

--- service-worker.js
+++ service-worker.js
@@ -12,9 +12,22 @@ const getConfig = (is_blocking) => {
         mode: "pac_script",
         pacScript: { data: is_blocking ? block_script : allow_script }
     };
-}; 
+};
 
-chrome.proxy.settings.set(
-    {value: getConfig(true), scope: 'regular'},
-    function() {}
-);
+const update_is_blocking = (is_blocking) => {
+    chrome.proxy.settings.set(
+        {value: getConfig(is_blocking), scope: 'regular'},
+        function() {}
+    );
+
+    console.log("Updated");
+}
+
+chrome.storage.local.get(["is_blocking"]).then((result) => {
+    if (result) update_is_blocking(result.is_blocking);
+    else {
+        chrome.storage.local.set({ is_blocking: true });
+        update_is_blocking(true);
+    }
+});
+chrome.storage.onChanged.addListener((changes) => update_is_blocking(changes.is_blocking.newValue));  
---
+++ settings.js
@@ -0,0 +1,5 @@
+const checkBoxEl = document.getElementById("checkbox1");
+chrome.storage.local.get(["is_blocking"]).then((result) => {
+    if (result) checkBoxEl.checked = result.is_blocking;
+});
+checkBoxEl.addEventListener("input", async (e) => await chrome.storage.local.set({ is_blocking: checkBoxEl.checked }));
